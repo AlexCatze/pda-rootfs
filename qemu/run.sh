ffdea2790e9dd516d7db2e435f260da6e3dd3c66
AlexCatze <alexcatze@gmail.com>
2022-12-02 19:42:08 +0200

    update

---
+++ qemu/run.sh
@@ -0,0 +1,1 @@
+qemu-system-arm -m 256 -M versatilepb -kernel ../workdir/versatile-zImage -append "root=/dev/sda2 rw console=ttyAMA0 console=tty0" -drive file=/dev/mmcblk0,if=scsi,format=raw
--- shared/rootfs/bin/oobe.sh
+++ shared/rootfs/bin/oobe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/xinput_calibrator --output-filename /usr/share/X11/xorg.conf.d/99-calibration.conf
+/bin/xinput_calibrator --no-timeout --output-filename /usr/share/X11/xorg.conf.d/99-calibration.conf
