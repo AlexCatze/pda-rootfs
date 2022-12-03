699bad85da28c250b462ba309d30cf849b6c41f2
AlexCatze <alexcatze@gmail.com>
2022-12-03 21:04:05 +0200

    fix

--- shared/rootfs/usr/bin/icewm-menu-wireless.sh
+++ shared/rootfs/usr/bin/icewm-menu-wireless.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if lsmod | grep "hx4700_acx" &> /dev/null ; then
-  echo "prog \"Disable WI-FI\" /usr/share/icons/Adwaita/32x32/devices/network-wired.png bash -c \"rmmod hx4700_acx; rmmod acx-mac80211\"\n"
-else
-  echo "prog \"Enable WI-FI\" /usr/share/icons/Adwaita/32x32/status/network-wired-disconnected.png bash -c \"insmod hx4700_acx; insmod acx-mac80211\"\n"
+
+
+file_count=$(find /lib/modules -name hx4700_acx.ko | wc -l)
+if [[ $file_count -gt 0 ]]; then
+  if lsmod | grep "hx4700_acx" &> /dev/null ; then
+    echo "prog \"Disable WI-FI\" /usr/share/icons/Adwaita/32x32/devices/network-wired.png bash -c \"rmmod hx4700_acx; rmmod acx-mac80211\"\n"
+  else
+    echo "prog \"Enable WI-FI\" /usr/share/icons/Adwaita/32x32/status/network-wired-disconnected.png bash -c \"modprobe acx-mac80211; modprobe hx4700_acx\"\n"
+  fi
 fi
